93079160a56914e6f64d27d7d28299cb6925472c
Sachi Udawaththa <83857134+SachiPU12@users.noreply.github.com>
2021-08-22 01:05:20 +0530

    Add files via upload

---
+++ My Profile/My_Profile.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	cout << "My Profile \n";
+    cout << "\t Name: Primalka Hansamali \n";
+    cout << "\t Age: 25 \n";
+    cout << "\t Gender: Female \n";
+    cout << "\t School: Anula Vidyalaya \n";
+    cout << "\t Address: Galle \n";
+    return 0;
+}
+
